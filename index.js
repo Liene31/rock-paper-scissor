e84dd538a89874aad16e655af4ff57705d5151db
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-07-08 22:14:06 +0200

    Add IDs to option images and stub JS functions
    Assigned unique IDs to rock, paper, and scissor images in index.html for easier DOM selection. Added initial function stubs and comments in index.js to outline game logic for handling user clicks, determining the winner, and displaying outcomes.

---
+++ index.js
@@ -0,0 +1,21 @@
+//When clicked one of the options
+//Hands are moving up and down
+//H2 text changes to Wait...
+
+//When it stops ->
+//Computer randomly choose the choise
+//Left side User
+//Right side Computer
+//The images changes to the made choises
+//Text changed according to winner -> User Won!! or Cpu Won!! or Match Draw
+
+//When clicked again, it starts again
+
+//---Random number
+//---Need to detected which option is clicked
+//---Implement game logic
+
+function handleClickes() {}
+function determinTheWinner() {}
+function gameLogic() {}
+function displayMatchOutcome() {}
