3fe32fe8796a23e5294a6fe07844ff4ef8553287
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-07-11 14:21:51 +0200

    Refactor game logic and fix element IDs
    Renamed 'scissor' to 'scissors' in HTML and JS for consistency. Added 'game-play' ID to the container. Refactored winnerDisplay and added handleAnimation logic into separate functions, and introduced displayChoice to show user/computer selections.

--- index.js
+++ index.js
@@ -17,6 +17,7 @@
 
 const options = document.querySelectorAll("#options > div");
 const gameMessage = document.getElementById("game-message");
+const gamePlay = document.getElementById("game-play");
 const rockLeft = document.getElementById("rock-left");
 const rockRight = document.getElementById("rock-right");
 
@@ -26,8 +27,26 @@ function determinTheWinner() {}
 function gameLogic() {}
 
 //Display the message of the winner - computer or user
-function displayWinner() {
+function displayWinnerMessage() {
   gameMessage.innerHTML = `<h1>Cpu Won!!</h1>`; // This is hardcoded for now
+}
+
+//Display the choice of the user and computer on the screen
+function displayChoice(option) {
+  gamePlay.innerHTML = `
+        <img
+          src="images/${option}.png"
+          alt="${option}-left"
+          class="rock-left"
+          id="rock-left"
+        />
+        
+         <img
+          src="images/${option}.png"
+          alt="${option}-right"
+          class="rock-right"
+          id="rock-right"
+        />`;
 }
 
 //Move hands up & down
@@ -42,19 +61,29 @@ function moveHands() {
   rockRight.style.transform = `rotate(-${degrees}deg) scaleX(-1)`;
 }
 
+//Handles the movement of hands + displays the message
+function handleAnimation() {
+  const handsInterval = setInterval(moveHands, 1000);
+  setTimeout(function () {
+    clearInterval(handsInterval);
+  }, 4000);
+  setTimeout(displayWinnerMessage, 4000);
+}
+
 //Handles clicks and determines which option was clicked
 options.forEach((option) => {
   option.addEventListener("click", (e) => {
     const optionId = e.currentTarget.id;
 
-    if (optionId === "rock" || optionId === "paper" || optionId === "scissor") {
+    if (
+      optionId === "rock" ||
+      optionId === "paper" ||
+      optionId === "scissors"
+    ) {
       gameMessage.innerHTML = `<h1>Wait...</h1>`;
     }
 
-    const handsInterval = setInterval(moveHands, 1000);
-    setTimeout(function () {
-      clearInterval(handsInterval);
-    }, 4000);
-    setTimeout(displayWinner, 4000);
+    handleAnimation();
+    displayChoice(optionId); // To check how to updated this
   });
 });
